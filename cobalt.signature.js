064262e8b58110137b3df15c67b0f7fa86a9fd93
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-15 10:23:29 +0200

    ready to handle event

--- cobalt.signature.js
+++ cobalt.signature.js
@@ -1,16 +1,22 @@
-(function(cobalt){
-    var plugin={
-        classes: {
-        	ios: 'SignaturePlugin',
-			android:'io.kristal.signature.Signature'
-        },
-        init:function(){
-            cobalt.signature=this.sign.bind(this);
-        },
-        sign:function(option,callback){
-            cobalt.plugins.send(this, "sign",option, callback);
-        }
-    };
-    cobalt.plugins.register(plugin);
+(function(cobalt) {
+  var plugin = {
+    classes: {
+      ios: 'SignaturePlugin',
+      android: 'io.kristal.signature.Signature'
+    },
+    init: function() {
+      cobalt.signature = this.sign.bind(this);
+    },
+    sign: function(option, callback) {
+      this.onSignatureResult = callback;
+      cobalt.plugins.send(this, "sign", option, callback);
+    },
+    handleEvent: function(json) {
+      if (typeof cobalt.onSignatureResult === 'function') {
+        cobalt.onSignatureResult(json.data)
+      }
+    }
+  };
+  cobalt.plugins.register(plugin);
 
 })(cobalt || {});
